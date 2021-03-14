a6a9f6f759ef0c355f1644250f4f65ff5b89e584
Damian Nogueiras <damiannogueiras@gmail.com>
2021-03-14 11:33:07 +0100

    fix model

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from odoo import models, fields, api
 
-from odoo import models, fields
 
 class Course(models.Model):
     _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
-#     _description = 'openacademy.openacademy'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
